bd8bd40f2c4ec5fbe8ed74545a176b911ceb5485
ishitaku5522 <ishitaku5522@gmail.com>
2022-11-22 14:22:51 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # golipper
 Simple clipboard server for ssh
+
+## Setup ssh config like following
+
+```
+Host server_name
+    HostName XXX.XXX.XXX.XXX
+    Port XXXX
+    User XXXX
+    IdentityFile ~/.ssh/xxx_id_rsa
+    ...
+    # Add following line
+    RemoteForward 8377 localhost:8377
+```
+
+## Launch golipper at local
+
+```sh
+# Local
+go run src/main.go
+```
+
+## Send to clipboard remotely
+
+```sh
+# Remote
+echo "hello world" | nc localhost 8377
+```
--- src/main.go
+++ src/main.go
@@ -8,149 +8,159 @@ import (
 	"os/exec"
 	"runtime"
 	"strconv"
+
 	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
 )
 
 type Configuration struct {
-    Port int
+	Port int
 }
 
 func main() {
-    os.Exit(mainModule())
+	os.Exit(mainModule())
 }
 
 func mainModule() int {
-     errorExitCode := 1
-    var config Configuration
-    err := envconfig.Process("golipper", &config)
-    if err != nil {
-        log.Fatal(err.Error())
-        return errorExitCode
-    }
+	errorExitCode := 1
+	var config Configuration
+	var port = 8377
+	err := envconfig.Process("golipper", &config)
+	if err != nil {
+		log.Fatal(err.Error())
+		return errorExitCode
+	}
 
-    if config.Port == 0 {
-        log.Fatal("Please define GOLIPPER_PORT environment variable.")
-        return errorExitCode
-    }
+	if config.Port == 0 {
+		log.Println("GOLIPPER_PORT environment variable is not defined. Using default port: " + strconv.Itoa(port))
+	} else {
+		port = config.Port
+	}
 
-    ln, err := net.Listen("tcp", ":" + strconv.Itoa(config.Port))
-    log.Println("Listening on localhost:" + strconv.Itoa(config.Port))
-    if err != nil {
-        log.Fatal(err.Error())
-        return errorExitCode
-    }
+	ln, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	log.Println("Listening on localhost:" + strconv.Itoa(port))
+	if err != nil {
+		log.Fatal(err.Error())
+		return errorExitCode
+	}
 
-    for {
-        conn, err := ln.Accept()
-        if err != nil {
-            log.Fatal(err.Error())
-            return errorExitCode
-        }
-        go handleConnection(conn)
-    }
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err.Error())
+			return errorExitCode
+		}
+		go handleConnection(conn)
+	}
 }
 
 func handleConnection(conn net.Conn) error {
-    defer conn.Close()
+	defer conn.Close()
 
-    buffer := make([]byte, 1024)
+	buffer := make([]byte, 1024)
 
-    message := ""
-    for {
-        n, err := conn.Read(buffer)
-        if err != nil {
-            if err != io.EOF {
-                return xerrors.Errorf("Connection read error: %w", err)
-            }
-            break
-        }
-        message = message + string(buffer[:n])
-        log.Print(message)
-    }
+	message := ""
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				return xerrors.Errorf("Connection read error: %w", err)
+			}
+			break
+		}
+		message = message + string(buffer[:n])
+		log.Print("Recieved message: " + string(buffer[:n]))
+	}
 
-    err := putStringToClipboard(message)
-    if err != nil {
-        return err
-    }
+	err := putStringToClipboard(message)
+	if err != nil {
+		return err
+	}
 
-    writeBytes := []byte("helloasdfasdf")
-    _, err = conn.Write(writeBytes)
+	writeBytes := []byte("helloasdfasdf")
+	_, err = conn.Write(writeBytes)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 func logOutputPipe(out io.ReadCloser) error {
-    defer out.Close()
-    buffer := make([]byte, 1024)
-    message := ""
+	defer out.Close()
+	buffer := make([]byte, 1024)
+	message := ""
 
-    for {
-        n, err := out.Read(buffer)
-        if err != nil {
-            if err != io.EOF {
-                log.Fatal("Output read error: %w", err)
-                return xerrors.Errorf("Output read error: %w", err)
-            }
-            break
-        }
-        message = message + string(buffer[:n])
-    }
+	for {
+		n, err := out.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal("Output read error: %w", err)
+				return xerrors.Errorf("Output read error: %w", err)
+			}
+			break
+		}
+		message = message + string(buffer[:n])
+	}
 
-    log.Print(message)
-    return nil
+	log.Print("Clip Command Log: " + message)
+	return nil
 }
 
 func putStringToClipboard(content string) error {
-    var clipCommand string
+	var clipCommand string
+	var cmd *exec.Cmd
 
-    log.Println("Putting string: \"" + content + "\" to clipboard")
+	log.Println("Putting string to clipboard: \n===== Content =====\n" + content)
 
-    clipCommand = "not found"
-    if runtime.GOOS == "windows" {
-        clipCommand = "clip.exe"
-    }
+	if runtime.GOOS == "windows" {
+		clipCommand = "clip.exe"
+		cmd = exec.Command(clipCommand)
+	} else if runtime.GOOS == "darwin" {
+		clipCommand = "pbcopy"
+		cmd = exec.Command("sh", "-c", clipCommand)
+	} else if runtime.GOOS == "linux" {
+		clipCommand = "xsel -ip && xsel -op | xsel -ib"
+		cmd = exec.Command("sh", "-c", clipCommand)
+	} else {
+		return xerrors.New("Unsupported platform error")
+	}
 
-    cmd := exec.Command(clipCommand)
-    stdin, err := cmd.StdinPipe()
+	stdin, err := cmd.StdinPipe()
 
-    if err != nil {
-        return xerrors.Errorf("Stdin pipe error: %w", err)
-    }
+	if err != nil {
+		return xerrors.Errorf("Stdin pipe error: %w", err)
+	}
 
-    go func() {
-        defer stdin.Close()
-        _, err := io.WriteString(stdin, content)
-        if err != nil {
-            log.Fatal(xerrors.Errorf("Stdin put error: %w", err).Error())
-        }
-    }()
+	go func() {
+		defer stdin.Close()
+		_, err := io.WriteString(stdin, content)
+		if err != nil {
+			log.Fatal(xerrors.Errorf("Stdin put error: %w", err).Error())
+		}
+	}()
 
-    stderr, err := cmd.StderrPipe()
-    if err != nil {
-        return xerrors.Errorf("Stderr pipe error: %w", err)
-    }
-    stdout, err := cmd.StdoutPipe()
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return xerrors.Errorf("Stderr pipe error: %w", err)
+	}
+	stdout, err := cmd.StdoutPipe()
 
-    if err != nil {
-        return xerrors.Errorf("Stdout pipe error: %w", err)
-    }
+	if err != nil {
+		return xerrors.Errorf("Stdout pipe error: %w", err)
+	}
 
-    go logOutputPipe(stderr)
-    go logOutputPipe(stdout)
+	go logOutputPipe(stderr)
+	go logOutputPipe(stdout)
 
+	cmd.Run()
+	if err != nil {
+		return xerrors.Errorf("Command run error: %w", err)
+	}
+	exitCode := cmd.ProcessState.ExitCode()
 
-    cmd.Run()
-    if err != nil {
-        return xerrors.Errorf("Command run error: %w", err)
-    }
-    exitCode := cmd.ProcessState.ExitCode()
+	log.Println("Exit code: " + strconv.Itoa(exitCode))
 
-    log.Println("Exit code: " + strconv.Itoa(exitCode))
-
-    return nil
+	return nil
 }
